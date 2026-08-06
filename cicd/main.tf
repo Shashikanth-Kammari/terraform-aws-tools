9aa5e613d81fa4c047efb0e31bd237786b2b82f8
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-06 21:44:06 +0530

    adding

--- cicd/main.tf
+++ cicd/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "tools" {
   key_name   = "tools"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/tools.pub")
+  public_key = file("~/.ssh/github.pub")
   # ~ means windows home directory
 }
 
@@ -41,7 +41,6 @@ module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "nexus"
-
   instance_type          = "t3.medium"
   vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
