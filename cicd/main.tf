aba333218ab480a9d16906f15df7a6805a0ff93e
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-06 21:51:29 +0530

    adding

--- cicd/main.tf
+++ cicd/main.tf
@@ -3,9 +3,9 @@ module "jenkins" {
 
   name = "jenkins-tf"
   key_name = aws_key_pair.tools.key_name
-  instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"] #replace your SG
-  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-00a444020c19fefae"] #replace your SG
+  subnet_id = "subnet-02d2c7fda57136081" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -13,87 +13,87 @@ module "jenkins" {
   }
 }
 
-module "jenkins_agent" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# module "jenkins_agent" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "jenkins-agent"
+#   name = "jenkins-agent"
 
-  instance_type          = "t3.small"
-  key_name = aws_key_pair.tools.key_name
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
-  # convert StringList to list and get first element
-  subnet_id = "subnet-0ea509ad4cba242d7"
-  ami = data.aws_ami.ami_info.id
-  user_data = file("jenkins-agent.sh")
-  tags = {
-    Name = "jenkins-agent"
-  }
-}
+#   instance_type          = "t3.small"
+#   key_name = aws_key_pair.tools.key_name
+#   vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0ea509ad4cba242d7"
+#   ami = data.aws_ami.ami_info.id
+#   user_data = file("jenkins-agent.sh")
+#   tags = {
+#     Name = "jenkins-agent"
+#   }
+# }
 
-resource "aws_key_pair" "tools" {
-  key_name   = "tools"
-  # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/github.pub")
-  # ~ means windows home directory
-}
+# resource "aws_key_pair" "tools" {
+#   key_name   = "tools"
+#   # you can paste the public key directly like this
+#   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+#   public_key = file("~/.ssh/github.pub")
+#   # ~ means windows home directory
+# }
 
-module "nexus" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+# module "nexus" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "nexus"
-  instance_type          = "t3.medium"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
-  # convert StringList to list and get first element
-  subnet_id = "subnet-0ea509ad4cba242d7"
-  ami = data.aws_ami.nexus_ami_info.id
-  key_name = aws_key_pair.tools.key_name
-  root_block_device = [
-    {
-      volume_type = "gp3"
-      volume_size = 30
-    }
-  ]
-  tags = {
-    Name = "nexus"
-  }
-}
+#   name = "nexus"
+#   instance_type          = "t3.medium"
+#   vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
+#   # convert StringList to list and get first element
+#   subnet_id = "subnet-0ea509ad4cba242d7"
+#   ami = data.aws_ami.nexus_ami_info.id
+#   key_name = aws_key_pair.tools.key_name
+#   root_block_device = [
+#     {
+#       volume_type = "gp3"
+#       volume_size = 30
+#     }
+#   ]
+#   tags = {
+#     Name = "nexus"
+#   }
+# }
 
-module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
-  version = "~> 2.0"
+# module "records" {
+#   source  = "terraform-aws-modules/route53/aws//modules/records"
+#   version = "~> 2.0"
 
-  zone_name = var.zone_name
+#   zone_name = var.zone_name
 
-  records = [
-    {
-      name    = "jenkins"
-      type    = "A"
-      ttl     = 1
-      records = [
-        module.jenkins.public_ip
-      ]
-      allow_overwrite = true
-    },
-    {
-      name    = "jenkins-agent"
-      type    = "A"
-      ttl     = 1
-      records = [
-        module.jenkins_agent.private_ip
-      ]
-      allow_overwrite = true
-    },
-    {
-      name    = "nexus"
-      type    = "A"
-      ttl     = 1
-      allow_overwrite = true
-      records = [
-        module.nexus.private_ip
-      ]
-      allow_overwrite = true
-    }
-  ]
+#   records = [
+#     {
+#       name    = "jenkins"
+#       type    = "A"
+#       ttl     = 1
+#       records = [
+#         module.jenkins.public_ip
+#       ]
+#       allow_overwrite = true
+#     },
+#     {
+#       name    = "jenkins-agent"
+#       type    = "A"
+#       ttl     = 1
+#       records = [
+#         module.jenkins_agent.private_ip
+#       ]
+#       allow_overwrite = true
+#     },
+#     {
+#       name    = "nexus"
+#       type    = "A"
+#       ttl     = 1
+#       allow_overwrite = true
+#       records = [
+#         module.nexus.private_ip
+#       ]
+#       allow_overwrite = true
+#     }
+#   ]
 
-}
+# }
