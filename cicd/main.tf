859098cfd671833697927cd9c72c6f5dea9364d4
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-11 22:57:27 +0530

    adding

--- cicd/main.tf
+++ cicd/main.tf
@@ -2,9 +2,10 @@ module "jenkins" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "jenkins-tf"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-00a444020c19fefae"] #replace your SG
-  subnet_id = "subnet-02d2c7fda57136081" #replace your Subnet
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"] #replace your SG
+  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -32,7 +33,7 @@ resource "aws_key_pair" "tools" {
   key_name   = "tools"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/github.pub")
+  public_key = file("~/.ssh/tools.pub")
   # ~ means windows home directory
 }
 
@@ -40,6 +41,7 @@ module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "nexus"
+
   instance_type          = "t3.medium"
   vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
