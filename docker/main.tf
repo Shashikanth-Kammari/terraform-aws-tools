c508163c337b5dae486c22cbd03b0697b2c35209
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-11 23:15:00 +0530

    adding

--- docker/main.tf
+++ docker/main.tf
@@ -3,7 +3,7 @@ module "docker" {
 
   name = "docker"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["vpc-0beeab66fb45f9217"] #replace your SG
+  vpc_security_group_ids = ["sg-00fa4a6a96d8a35ca"] #replace your SG
   subnet_id = "subnet-03f20ffb2c0cdbe1d" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("docker.sh")
