21b30992bfd9454213976cec5e8a8bb3830fd10c
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-17 23:13:11 +0530

    adding

--- docker/docker.sh
+++ docker/docker.sh
@@ -17,6 +17,7 @@ sudo dnf -y install dnf-plugins-core
 
 echo "downloading docker repo "
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+echo "successfully downloaded docker repo"
 
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
