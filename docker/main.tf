c48cf3ade8410651b72643c393f2fa4324cfcfc6
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-17 23:10:23 +0530

    adding

--- docker/main.tf
+++ docker/main.tf
@@ -2,7 +2,7 @@ module "docker" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "docker"
-  instance_type          = "t3.micro"
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-00fa4a6a96d8a35ca"] #replace your SG
   subnet_id = "subnet-03f20ffb2c0cdbe1d" #replace your Subnet
   ami = data.aws_ami.ami_info.id
